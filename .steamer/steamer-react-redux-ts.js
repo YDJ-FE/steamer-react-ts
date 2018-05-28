61e8da8e71bf6c1bf052fc7d7a624d2c2ecc4084
vega <vegawong1024@gmail.com>
2018-05-28 09:27:25 +0800

    up 添加钩子

--- .steamer/steamer-react-redux-ts.js
+++ .steamer/steamer-react-redux-ts.js
@@ -16,24 +16,47 @@ module.exports = {
         "tsconfig.json",
         "tslint.json"
     ],
+    beforeCopy: function(answers, folderPath) {
+        if(!!answers.jest) {
+            this.files.push('test')
+        }
+    },
+    beforeInstall: function(answers, folderPath) {
+        if (!!answers.jest) {
+            let pkg = this.getPkgJson(folderPath);
+            pkg.dependencies = Object.assign({}, pkg.dependencies, {
+                enzyme: "^3.3.0",
+                jest: "^22.4.4"
+            });
+            pkg.devDependencies = Object.assign({}, pkg.devDependencies, {
+                "@types/enzyme": "^3.1.10",
+                "@types/jest": "^22.2.3",
+                "enzyme-adapter-react-15": "^1.0.5"
+            });
+            pkg.scripts = Object.assign({}, pkg.scripts, {
+                test: "jest",
+                coverage: "jest --coverage"
+            });
+        }
+    },
     options: [
         {
-            type: 'input',
-            name: 'webserver',
-            message: 'html url(//localhost:9000/)',
-            default: "//localhost:9000/",
+            type: "input",
+            name: "webserver",
+            message: "html url(//localhost:9000/)",
+            default: "//localhost:9000/"
         },
         {
-            type: 'input',
-            name: 'cdn',
-            message: 'common cdn url(//ss.yidejia.com/)',
-            default: "//ss.yidejia.com/",
+            type: "input",
+            name: "cdn",
+            message: "common cdn url(//ss.yidejia.com/)",
+            default: "//ss.yidejia.com/"
         },
         {
-            type: 'input',
-            name: 'port',
-            message: 'development server port(9000)',
-            default: '9000',
+            type: "input",
+            name: "port",
+            message: "development server port(9000)",
+            default: "9000"
         }
     ]
 };
