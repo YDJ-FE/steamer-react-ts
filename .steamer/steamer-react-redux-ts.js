805442f183167f785d0202b8cad6423190e4b7d9
vega <vegawong1024@gmail.com>
2018-06-15 15:10:38 +0800

    up 修复update功能， 修改测试文档

--- .steamer/steamer-react-redux-ts.js
+++ .steamer/steamer-react-redux-ts.js
@@ -2,93 +2,103 @@ const fs = require('fs')
 const path = require('path')
 const replaceReduxFilesToMobx = require('./replace-redux-files-to-mobx')
 
-const files = [
-    'config',
-    'src',
-    'tools',
-    'typings',
-    '.gitignore',
-    '.npmrc',
-    '.prettierrc',
-    '.stylelintignore',
-    '.stylelintrc.js',
-    'package.json',
-    'postcss.config.js',
-    'qshell.qa.json',
-    'qshell.prod.json',
-    'README.md',
-    'tsconfig.json',
-    'tslint.json'
-]
+const onInstall = function(folderPath, config) {
+    const pkg = this.getPkgJson(folderPath)
+    let pkgHasChanged = false
+    if (!!config.mobx) {
+        pkgHasChanged = true
+        pkg.dependencies = Object.assign({}, pkg.dependencies, {
+            mobx: '^5.0.0',
+            'mobx-react': '^5.2.2'
+        })
+        const rmMods = {
+            dependencies: [
+                'react-redux',
+                'react-router-redux',
+                'redux',
+                'redux-immutable',
+                'redux-saga',
+                'reselect'
+            ],
+            devDependencies: [
+                '@types/react-redux',
+                '@types/react-router-redux'
+            ]
+        }
+        for (const dep of Object.keys(rmMods)) {
+            for (const mod of rmMods[dep]) {
+                delete pkg[dep][mod]
+            }
+        }
+        replaceReduxFilesToMobx.bind(this)(folderPath)
+    }
+    if (!!config.jest) {
+        pkgHasChanged = true
+        pkg.dependencies = Object.assign({}, pkg.dependencies, {
+            enzyme: '^3.3.0',
+            jest: '^22.4.4'
+        })
+        pkg.devDependencies = Object.assign({}, pkg.devDependencies, {
+            '@types/enzyme': '^3.1.10',
+            '@types/jest': '^22.2.3',
+            'enzyme-adapter-react-16': '^1.1.1'
+        })
+        pkg.scripts = Object.assign({}, pkg.scripts, {
+            test: 'jest',
+            coverage: 'jest --coverage'
+        })
+    }
+    if (pkgHasChanged) {
+        fs.writeFileSync(
+            path.join(folderPath, 'package.json'),
+            JSON.stringify(pkg, null, 4),
+            'utf-8'
+        )
+    }
+    this.walkAndReplace(
+        folderPath,
+        ['.js', '.jsx', '.ts', '.tsx', '.html', '.json'],
+        {
+            ProjectName: config.projectName.replace(/^[a-z]/, l => l.toUpperCase()),
+            projectName: config.projectName.replace(/^[A-Z]/, L => L.toLowerCase()),
+        }
+    )
+}
 
 module.exports = {
-    files: files,
-    beforeInstallCopy: function(answers, folderPath) {
+    files: [
+        'config',
+        'src',
+        'tools',
+        'typings',
+        '.gitignore',
+        '.npmrc',
+        '.prettierrc',
+        '.stylelintignore',
+        '.stylelintrc.js',
+        'package.json',
+        'postcss.config.js',
+        'qshell.qa.json',
+        'qshell.prod.json',
+        'README.md',
+        'tsconfig.json',
+        'tslint.json'
+    ],
+    beforeInstallCopy: function(answers, folderPath, files) {
+        console.log('------------- beforeInstallCopy -------------');
         if (!!answers.jest) {
             files.push('test', 'jest.config.js')
         }
     },
     beforeInstallDep: function(answers, folderPath) {
-        const pkg = this.getPkgJson(folderPath)
-        let pkgHasChanged = false
-        if (!!answers.mobx) {
-            pkgHasChanged = true
-            pkg.dependencies = Object.assign({}, pkg.dependencies, {
-                mobx: '^5.0.0',
-                'mobx-react': '^5.2.2'
-            })
-            const rmMods = {
-                dependencies: [
-                    'react-redux',
-                    'react-router-redux',
-                    'redux',
-                    'redux-immutable',
-                    'redux-saga',
-                    'reselect'
-                ],
-                devDependencies: [
-                    '@types/react-redux',
-                    '@types/react-router-redux'
-                ]
-            }
-            for (const dep of Object.keys(rmMods)) {
-                for (const mod of rmMods[dep]) {
-                    delete pkg[dep][mod]
-                }
-            }
-            replaceReduxFilesToMobx.bind(this)(folderPath)
-        }
-        if (!!answers.jest) {
-            pkgHasChanged = true
-            pkg.dependencies = Object.assign({}, pkg.dependencies, {
-                enzyme: '^3.3.0',
-                jest: '^22.4.4'
-            })
-            pkg.devDependencies = Object.assign({}, pkg.devDependencies, {
-                '@types/enzyme': '^3.1.10',
-                '@types/jest': '^22.2.3',
-                'enzyme-adapter-react-16': '^1.1.1'
-            })
-            pkg.scripts = Object.assign({}, pkg.scripts, {
-                test: 'jest',
-                coverage: 'jest --coverage'
-            })
-        }
-        if (pkgHasChanged) {
-            fs.writeFileSync(
-                path.join(folderPath, 'package.json'),
-                JSON.stringify(pkg, null, 4),
-                'utf-8'
-            )
-        }
-        this.walkAndReplace(
-            folderPath,
-            ['.js', '.jsx', '.ts', '.tsx', '.html', '.json'],
-            {
-                ProjectName: answers.projectName.replace(/^[a-z]/, l => l.toUpperCase()),
-                projectName: answers.projectName.replace(/^[A-Z]/, L => L.toLowerCase()),
-            }
-        )
+        console.log('------------- beforeInstallDep -------------');
+        onInstall.bind(this)(folderPath, answers);
+    },
+    beforeUpdateDep: function() {
+        console.log('------------- beforeUpdateDep -------------');
+        const folderPath = process.cwd();
+        const answersConfig = this.readKitConfig(path.join(folderPath, 'config/steamer.config.js'));
+        onInstall.bind(this)(folderPath, answersConfig);
     },
     options: [
         {
--- docs/test.md
+++ docs/test.md
@@ -1,7 +1,11 @@
 # 测试
 
 ``` shell
-$ cd .kit-test
-$ npm i
-$ npm test
+$ steamer kit -d [aliasName]
+$ cd <newPath>
+$ steamer kit 
 ```
+
+通过`steamer kit -d [aliasName]`会生成一个软链接在`~/.steamer/starterkits/`， 并已当前的版本号命令产生一个分支
+
+然后可以一边开发， 一边在新的folder里面init项目
