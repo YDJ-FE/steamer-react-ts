cf6d5daa6a652471c78412c759deebc77cf45118
shanghaitao <359310444@qq.com>
2018-07-17 16:47:43 +0800

    up add svg typing and modify file path

--- .steamer/support-svg-component.js
+++ .steamer/support-svg-component.js
@@ -5,9 +5,19 @@ const svgModifiedPath = path.join(__dirname, '.svgModified')
 module.exports = function(folderPath) {
     // 修改fileRules(添加svg loader)
     const fileRulesPath = path.join(folderPath, 'tools/rules/fileRules.js')
-    this.fs.copySync(path.join(svgModifiedPath, 'fileRules.js'), fileRulesPath)
+    this.fs.copySync(
+        path.join(svgModifiedPath, 'tools/rules/fileRules.js'),
+        fileRulesPath
+    )
 
     // 修改入口scss
     const scssPath = path.join(folderPath, 'src/app.scss')
-    this.fs.copySync(path.join(svgModifiedPath, 'app.scss'), scssPath)
+    this.fs.copySync(path.join(svgModifiedPath, 'src/app.scss'), scssPath)
+
+    // 添加svg typing
+    const svgTypingPath = path.join(folderPath, 'typings/svg.d.ts')
+    this.fs.copySync(
+        path.join(svgModifiedPath, 'typings/svg.d.ts'),
+        svgTypingPath
+    )
 }
