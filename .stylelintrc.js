7654ea7006470509a33abe022ec42d1d563c51fb
vega <vegawong1024@gmail.com>
2018-08-17 14:36:40 +0800

    fix: lintstaged config file add to init project

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,7 +2,8 @@ module.exports = {
     extends: ['stylelint-config-ydj/scss', 'stylelint-config-ydj/prettier', 'stylelint-config-css-modules'],
     ignoreFiles: [
         '**/*.md',
-        '**/*.{ts, tsx}',
+        '**/*.ts',
+        '**/*.tsx',
         '**/*.js'
     ]
 }
